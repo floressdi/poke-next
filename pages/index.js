afe212d117b29a54b8bbfc593646773ebca02577
Diego <del.angelfull@gmailcom>
2023-06-13 14:33:01 -0600

    add color is type

--- pages/index.js
+++ pages/index.js
@@ -17,9 +17,11 @@ export default function Home({
   tipos,
   notFound,
 }) {
+
+
   const [filtro, setFiltrar] = useState(cardPokemoninfo);
 
-  const [clickedMenu, setClickedMenu] = useState(false); //maenjaremos el estado del menu
+  const [clickedMenu, setClickedMenu] = useState(false); //manejaremos el estado del menu
   const handleClickMenu = () => {
     setClickedMenu(!clickedMenu);
   };
@@ -27,21 +29,21 @@ export default function Home({
   const filtrar = (elTipo) => {
     setFiltrar(cardPokemoninfo);
 
-    if (elTipo === "borrar") {
-      setFiltrar(cardPokemoninfo);
-      setClickedMenu(!clickedMenu); //acutaliza el estado del click en el menu
-    } else {
-      setClickedMenu(!clickedMenu); //acutualiza el estado del click en el menu
-      let filtradoPorTipo = cardPokemoninfo
-        .filter((pokemon) =>
-          pokemon.types.some((tipo) => tipo.type.name === elTipo)
-        )
-        .map((tem2) => {
-          let nuevosTem = { ...tem2 };
-          return nuevosTem;
-        });
-      setFiltrar(filtradoPorTipo);
-    }
+      if (elTipo === "borrar") {
+        setFiltrar(cardPokemoninfo);
+        setClickedMenu(!clickedMenu); //acutaliza el estado del click en el menu
+      } else {
+        setClickedMenu(!clickedMenu); //acutualiza el estado del click en el menu
+        let filtradoPorTipo = cardPokemoninfo
+          .filter((pokemon) =>
+            pokemon.types.some((tipo) => tipo.type.name === elTipo)
+          )
+          .map((tem2) => {
+            let nuevosTem = { ...tem2 };
+            return nuevosTem;
+          });
+        setFiltrar(filtradoPorTipo);
+      }
   };
 
   return (
@@ -56,17 +58,17 @@ export default function Home({
         <div>
           <ul className={`ul ${clickedMenu ? "active" : ""}`}>
             <button
-              className="text-2xl ml-14 px-4 my-3 md:ml-72  hover:bg-blue-300 transition-all hover:pl-3 hover:text-cyan-700 "
+              className="w-11/12 mx-auto rounded-xl  text-2xl md:ml-14 p-2 ml-5 my-3 todos "
               onClick={() => filtrar("borrar")}
             >
               Mostrar Todos
             </button>
-            <div className="w-6/12 grid gap-3 ">
+            <div className="w-11/12 grid gap-3 mx-auto  ">
               {tipos.map((tipo, index) => {
                 return (
                   <button
                     key={tipo.name}
-                    className="text-2xl text-left ml-14 md:ml-72 px-4 cursor-pointer hover:bg-blue-300 transition-all hover:pl-3 hover:text-cyan-700"
+                    className={`${tipo.name} p-2 rounded-xl text-2xl`}
                     onClick={() => filtrar(tipo.name)}
                   >
                     {tipo.name}
@@ -101,6 +103,14 @@ export default function Home({
               ))
             : "Cargando..."}
         </div>
+
+        {/* <div className="w-full my-16  flex justify-center  h-auto">
+          <button onClick={funcion} className="text-center w-64 h-14  bg-yellow-300  rounded-lg">
+            Mostrar mas pokemons
+          </button>
+        </div> */}
+
+
       </section>
 
       <footer className="w-full">
@@ -117,16 +127,25 @@ export const getStaticProps = async (context) => {
   const getPokemon = async (indice) => {
     //Traemos pokemones de acuerdo al indicie
     const response = await fetch(
-      `https://pokeapi.co/api/v2/pokemon/${indice}?limit=204&offset=0/` //Pasamos el indice ala api
+      `https://pokeapi.co/api/v2/pokemon/${indice}?limit=704&offset=0/` //Pasamos el indice ala api
     );
     const data = await response.json();
     return data;
   };
 
   let pokemons = []; //Este arreglo guardara los pokemones
-  for (let indice = 1; indice <= 204; indice++) {
-    let data = await getPokemon(indice); //Ejecutamos getPokemon pasando como parametro el indice
-    pokemons.push(data); //los guarda en el arreglo
+
+  let inicio = 1
+  let limite = 204
+
+  // if(context == true){ // si mostrar mas es verdadero aumenta el limite 
+  //    limite += limite;
+  // }  
+
+  for (let indice = inicio; indice <= limite; indice++) {
+    let data = await getPokemon(indice);//Ejecutamos getPokemon pasando como parametro el indice
+    pokemons.push(data);//los guarda en el arreglo
+    inicio = limite;
   }
 
   let cardPokemoninfo = pokemons.map((pokemon) => {
